b2edc27b3f69ed2bfce13c107b9ef596c8a3f6ac
Paulo <paulo.phlp@outlook.com>
2020-07-28 10:01:42 -0300

    Chore: Start to add SigIn page.

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Logon into GoBarber</Title>
+
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input
+        name="password"
+        icon="lock"
+        placeholder="Password"
+        secureTextEntry
+      />
+
+      <Button
+        onPress={() => {
+          console.log('iauza');
+        }}
+      >
+        Login
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
